e88435914cc63441a42f2f367f5232fd623b74a3
Aswinpashok <aswinpashok4@gmail.com>
2017-03-16 14:02:35 +0530

    Added Color Customization

--- app/src/main/java/clasher/customalert/CustomAlert.java
+++ app/src/main/java/clasher/customalert/CustomAlert.java
@@ -4,11 +4,17 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.graphics.Typeface;
-import android.os.Environment;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.io.BufferedOutputStream;
@@ -17,56 +23,138 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-
 /**
- * Created by PC4 on 3/14/2017.
+ * Created by PC4 on 3/15/2017.
  */
 
-public class CustomAlert extends Activity {
-    TextView title_view,message_view;
-    public static final String TAG="lib###";
-    Button alertButton;
-    String title,message,ButtonText;
+public class CustomAlert {
+    private TextView title_view,message_view;
+    private Button alertButton;
     Activity activity;
-    Dialog dialog;
-    public CustomAlert(Activity activity,String title, String message, String buttonText) {
-        this.title = title;
-        this.message = message;
-        ButtonText = buttonText;
+    private Dialog dialog;
+    private RelativeLayout title_block;
+    CustomAlert alert;
+
+    public CustomAlert(Activity activity) {
         this.activity = activity;
         dialog=new Dialog(activity);
         dialog.setContentView(R.layout.alert_layout);
         alertButton=(Button) dialog.findViewById(R.id.AlertButton);
         title_view=(TextView) dialog.findViewById(R.id.title);
         message_view=(TextView) dialog.findViewById(R.id.AlertMessage);
-        alertButton.setText(buttonText);
-        title_view.setText(title);
-        message_view.setText(message);
+        title_block=(RelativeLayout) dialog.findViewById(R.id.title_block);
+
         Typeface titleface=getFontFromRes(R.raw.roboto_regular,activity);
         title_view.setTypeface(titleface);
         Typeface alertface=getFontFromRes(R.raw.roboto_light,activity);
         alertButton.setTypeface(alertface);
         message_view.setTypeface(alertface);
-        alertButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.dismiss();
-            }
-        });
-    }
-    public void show(){
-        dialog.show();
+        alert=this;
     }
 
-    private Typeface getFontFromRes(int resource, Context context)
-    {
+    public clasher.customalert.CustomAlert show(){
+        alert.dialog.show();
+        return alert;
+    }
+
+    public View getAlertButton(){
+        return alertButton;
+    }
+
+    public void close(){
+        dialog.dismiss();
+    }
+
+    public clasher.customalert.CustomAlert setTitle(String title){
+        try {
+            alert.title_view.setText(title);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return alert;
+    }
+
+    public clasher.customalert.CustomAlert setMessage(String message){
+        try {
+            alert.message_view.setText(message);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return alert;
+    }
+
+    public clasher.customalert.CustomAlert setButtonText(String buttonText){
+        try {
+            alert.alertButton.setText(buttonText);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return alert;
+    }
+
+    public clasher.customalert.CustomAlert setTitleBlockColor(String color){
+        try {
+            alert.title_block.setBackgroundColor(Color.parseColor(color));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return alert;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    public clasher.customalert.CustomAlert setButtonColor(String color) {
+        try {
+//            alert.alertButton.setBackgroundColor(Color.parseColor(color));
+//            Drawable drawable=alert.alertButton.getBackground();
+//            drawable.setColorFilter(0,PorterDuff.Mode.CLEAR);
+//            drawable.setColorFilter(Color.parseColor(color), PorterDuff.Mode.MULTIPLY);
+//            alertButton.setBackground(drawable);
+            GradientDrawable drawable=(GradientDrawable) alertButton.getBackground();
+            drawable.setColor(Color.parseColor(color));
+            alertButton.setBackground(drawable);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return alert;
+    }
+
+
+    public clasher.customalert.CustomAlert setButtonTextColor(String color) {
+        try {
+            alert.alertButton.setTextColor(Color.parseColor(color));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return alert;
+    }
+
+    public clasher.customalert.CustomAlert setTitleColor(String color){
+        try {
+            alert.title_view.setTextColor(Color.parseColor(color));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return alert;
+    }
+
+    public clasher.customalert.CustomAlert setMessageColor(String color){
+        try {
+            alert.message_view.setTextColor(Color.parseColor(color));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return alert;
+    }
+
+    private Typeface getFontFromRes(int resource, Context context) {
         Typeface tf = null;
         InputStream is = null;
         try {
             is = context.getResources().openRawResource(resource);
         }
         catch(Resources.NotFoundException e) {
-            Log.e(TAG, "Could not find font in resources!");
         }
 
         String outPath = context.getCacheDir() + "/tmp" + System.currentTimeMillis() +".raw";
@@ -89,14 +177,10 @@ public class CustomAlert extends Activity {
         }
         catch (IOException e)
         {
-            Log.e(TAG, "Error reading in font!");
             return null;
         }
 
-        Log.d(TAG, "Successfully loaded font.");
-
         return tf;
     }
-
 
 }
